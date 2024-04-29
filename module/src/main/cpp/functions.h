6926eb5d22a1683ce3d672140aa8e0eefe0fed0f
TYL0102 <101940089+TYL0102@users.noreply.github.com>
2024-04-30 01:41:01 +0800

    Update functions.h

--- module/src/main/cpp/functions.h
+++ module/src/main/cpp/functions.h
@@ -3,7 +3,7 @@
 
 // here you can define variables for the patches
 bool addCurrency, freeItems, everythingUnlocked, showAllItems, addSkins;
-bool allWeaponsTrained = true;
+bool allWeaponsTrained = true, trainAllCharacter = true;
 
 monoString *CreateIl2cppString(const char *str) {
     monoString *(*String_CreateString)(void *instance, const char *str) = (monoString*(*)(void*, const char*)) (g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0x2596B20")));
